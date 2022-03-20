6380f883dd252b7261628734a8407920d00487a7
862826044 <862826044@qq.com>
2022-03-20 14:30:45 +0800

    frist commit

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ use std::env;
 fn main() {
     print!("请输入你的姓名：");
     let args: Vec<String> = env::args().collect();
-    println!("传入了{:?}个参数", args.len());
+    println!("传入了{:?}个参数", args.len()-1);
     println!("你好！{:?}!", args[1]);
 }
